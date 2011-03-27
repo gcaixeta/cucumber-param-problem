5646be74f9bd0bac9a92b9179039aa8349a94a23
Gustavo Caixeta <gustavocaixeta@gmail.com>
2011-03-27 13:21:44 -0300

    change default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CucumberParamProblem::Application.routes.draw do
   resources :categories
+  root :to => "categories#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
